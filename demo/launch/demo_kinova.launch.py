94dcb288baba8c6d93e99d3f46eb15172a70cea9
Martin Stigleitner <martin.stigleitner@gmail.com>
2024-06-14 11:09:33 +0200

    Added demo kinova

---
+++ demo/launch/demo_kinova.launch.py
@@ -0,0 +1,473 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+from moveit_configs_utils import MoveItConfigsBuilder
+
+#############################################################################################
+# Crobotics /root/kortex2_ws/src/ros2_kortex/kortex_moveit_config/kinova_gen3_7dof_robotiq_2f_85_moveit_config/launch/sim.launch.py
+import yaml
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.conditions import IfCondition
+
+from ament_index_python.packages import get_package_share_directory
+
+from moveit_configs_utils import MoveItConfigsBuilder
+#############################################################################################
+
+
+#############################################################################################
+# dodao iz robot.launch.py
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import (
+    DeclareLaunchArgument,
+    OpaqueFunction,
+    RegisterEventHandler,
+)
+from launch.event_handlers import OnProcessExit
+from launch.conditions import IfCondition
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from moveit_configs_utils import MoveItConfigsBuilder
+#############################################################################################
+
+
+
+
+#############################################################################################
+# Crobotics /root/kortex2_ws/src/ros2_kortex/kortex_moveit_config/kinova_gen3_7dof_robotiq_2f_85_moveit_config/launch/sim.launch.py
+def load_yaml(package_name: str, file_path: str):
+    """
+    Load yaml configuration based on package name and file path relative to its share.
+    """
+
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+    return parse_yaml(absolute_file_path)
+
+def parse_yaml(absolute_file_path: str):
+    """
+    Parse yaml from file, given its absolute file path.
+    """
+
+    try:
+        with open(absolute_file_path, "r") as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:
+        return None
+#############################################################################################
+
+
+
+# orginalno
+#def generate_launch_description(): 
+
+#############################################################################################
+# dodao iz robot.launch.py
+def launch_setup(context, *args, **kwargs):
+#############################################################################################
+
+
+    #############################################################################################
+    # Crobotics /root/kortex2_ws/src/ros2_kortex/kortex_moveit_config/kinova_gen3_7dof_robotiq_2f_85_moveit_config/launch/sim.launch.py
+    moveit_config_package = "kinova_gen3_7dof_robotiq_2f_85_moveit_config"
+
+    # Initialize Arguments
+    launch_rviz = LaunchConfiguration("launch_rviz")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    sim_ignition = LaunchConfiguration("sim_ignition")
+    vision = LaunchConfiguration("vision") 
+    #############################################################################################
+
+
+
+    #############################################################################################
+    # dodao iz robot.launch.py
+    # Initialize Arguments
+    robot_ip = LaunchConfiguration("robot_ip")
+    use_fake_hardware = LaunchConfiguration("use_fake_hardware")
+    gripper_max_velocity = LaunchConfiguration("gripper_max_velocity")
+    gripper_max_force = LaunchConfiguration("gripper_max_force")
+    launch_rviz = LaunchConfiguration("launch_rviz")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    use_internal_bus_gripper_comm = LaunchConfiguration("use_internal_bus_gripper_comm")
+    #############################################################################################
+
+
+
+    #############################################################################################
+    # dodao iz robot.launch.py
+    launch_arguments = {
+        "robot_ip": robot_ip,
+        "use_fake_hardware": use_fake_hardware,
+        "gripper": "robotiq_2f_85",
+        "gripper_joint_name": "robotiq_85_left_knuckle_joint",
+        "dof": "7",
+        "gripper_max_velocity": gripper_max_velocity,
+        "gripper_max_force": gripper_max_force,
+        "use_internal_bus_gripper_comm": use_internal_bus_gripper_comm,
+    }
+    #############################################################################################
+
+
+
+
+    #############################################################################################
+    # dodao iz robot.launch.py
+    moveit_config = (
+        MoveItConfigsBuilder("gen3", package_name="kinova_gen3_7dof_robotiq_2f_85_moveit_config")
+        .robot_description(mappings=launch_arguments)
+        .planning_pipelines(pipelines=["ompl"])
+        .to_moveit_configs()
+    )
+    #############################################################################################
+
+
+    #############################################################################################
+    # Crobotics /root/kortex2_ws/src/ros2_kortex/kortex_moveit_config/kinova_gen3_7dof_robotiq_2f_85_moveit_config/launch/sim.launch.py
+    planning_pipeline = {
+        "planning_pipelines": ["ompl"],
+        "default_planning_pipeline": "ompl",
+        "ompl": {
+            "planning_plugin": "ompl_interface/OMPLPlanner",
+            # TODO: Re-enable `default_planner_request_adapters/AddRuckigTrajectorySmoothing` once its issues are resolved
+            "request_adapters": "default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/ResolveConstraintFrames default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints",
+            # TODO: Reduce start_state_max_bounds_error once spawning with specific joint configuration is enabled
+            "start_state_max_bounds_error": 0.31416,
+        },
+    }
+
+    _ompl_yaml = load_yaml(
+        moveit_config_package, os.path.join("config", "ompl_planning.yaml")
+    )
+
+    planning_pipeline["ompl"].update(_ompl_yaml)
+    #############################################################################################
+
+
+    publish_robot_description_semantic = {"publish_robot_description_semantic": True}
+    publish_robot_description = {"publish_robot_description": True}
+    publish_robot_description_kinematics = {"publish_robot_description_kinematics": True} 
+
+
+    #############################################################################################
+    # dodao iz robot.launch.py
+    moveit_config.moveit_cpp.update({"use_sim_time": use_sim_time.perform(context) == "true"})
+
+    move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[
+            moveit_config.to_dict(),
+            {"use_sim_time": use_sim_time}, 
+            publish_robot_description, 
+            publish_robot_description_kinematics, 
+            publish_robot_description_semantic,                     
+            planning_pipeline, 
+        ],
+        arguments=[
+            "--ros-args",
+            "--log-level",
+            "fatal",
+        ],  # MoveIt is spamming the log because of unknown '*_mimic' joints
+        condition=IfCondition(launch_rviz),
+    )
+    #############################################################################################
+
+
+
+
+    # Load  ExecuteTaskSolutionCapability so we can execute found solutions in simulation
+    move_group_capabilities = {"capabilities": "move_group/ExecuteTaskSolutionCapability"}
+
+    # Start the actual move_group node/action server
+    run_move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[
+            moveit_config.to_dict(),
+            move_group_capabilities,
+        ],
+    )
+
+
+
+    #############################################################################################
+    # Static TF
+    static_tf = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_transform_publisher",
+        output="log",
+        arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world", "base_link"],
+    )
+    #############################################################################################
+    
+    
+    #############################################################################################
+    """ 
+    zamjenio 
+    moveit_task_constructor_demo 
+
+    sa 
+    kinova_gen3_7dof_robotiq_2f_85_moveit_config
+
+    dodao 
+    /root/kortex2_ws/src/ros2_kortex/kortex_moveit_config/
+    kinova_gen3_7dof_robotiq_2f_85_moveit_config/config/view_robot.rviz
+
+    u 
+    kortex2_ws/src/ros2_kortex/kortex_moveit_config/kinova_gen3_7dof_robotiq_2f_85_moveit_config/config 
+    
+    i modificirao prema 
+    /root/kortex2_ws/src/ros2_kortex/kortex_moveit_config/kinova_gen3_7dof_robotiq_2f_85_moveit_config/config/mtc.rviz
+
+    """
+    # RViz
+    rviz_config_file = (
+        get_package_share_directory("kinova_gen3_7dof_robotiq_2f_85_moveit_config") + "/config/real_kinova.rviz"
+    )
+
+    # modificirao iz robot.launch.py
+
+    rviz_node = Node(
+        package="rviz2",
+        condition=IfCondition(launch_rviz),
+        executable="rviz2",
+        name="rviz2_moveit",
+        output="log",
+        arguments=["-d", rviz_config_file],
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            moveit_config.planning_pipelines,
+        ],
+    )
+    #############################################################################################
+    
+    
+    #############################################################################################
+    # dodao iz robot.launch.py
+    # Static TF
+    static_tf = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_transform_publisher",
+        output="log",
+        arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world", "base_link"],
+    )
+    #############################################################################################
+
+
+    #############################################################################################
+    # dodao iz robot.launch.py
+    # Publish TF
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
+        parameters=[
+            moveit_config.robot_description,
+        ],
+    )
+    #############################################################################################
+
+
+    #############################################################################################
+    # dodao iz robot.launch.py
+    # ros2_control using FakeSystem as hardware
+    ros2_controllers_path = os.path.join(
+        get_package_share_directory("kinova_gen3_7dof_robotiq_2f_85_moveit_config"),
+        "config",
+        "ros2_controllers.yaml",
+    )
+    #############################################################################################
+
+
+    #############################################################################################
+    """ 
+    promjenio
+    moveit_config.to_dict() 
+    u
+    moveit_config.robot_description 
+    """
+    # dodao iz robot.launch.py
+    ros2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[moveit_config.robot_description, ros2_controllers_path],
+        output="both",
+    )
+    #############################################################################################
+
+
+
+    #############################################################################################
+    # 4 dodao iz robot.launch.py
+    robot_traj_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_trajectory_controller", "-c", "/controller_manager"],
+    )
+    robot_pos_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["twist_controller", "--inactive", "-c", "/controller_manager"],
+    )
+    robot_hand_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["robotiq_gripper_controller", "-c", "/controller_manager"],
+        # condition=IfCondition(use_internal_bus_gripper_comm),
+    )
+    fault_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["fault_controller", "-c", "/controller_manager"],
+    )
+    #############################################################################################
+
+
+    # Load controllers
+    load_controllers = []
+    for controller in [
+        "panda_arm_controller",
+        "panda_hand_controller",
+        "joint_state_broadcaster",
+    ]:
+        load_controllers += [
+            ExecuteProcess(
+                cmd=["ros2 run controller_manager spawner {}".format(controller)],
+                shell=True,
+                output="screen",
+            )
+        ]
+
+
+
+
+
+    #############################################################################################
+    # 3 dodao iz robot.launch.py
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
+
+    # Delay rviz start after `joint_state_broadcaster`
+    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[rviz_node],
+        ),
+        condition=IfCondition(launch_rviz),
+    )
+
+    nodes_to_start = [
+        ros2_control_node,
+        robot_state_publisher,
+        joint_state_broadcaster_spawner,
+        delay_rviz_after_joint_state_broadcaster_spawner,
+        robot_traj_controller_spawner,
+        robot_pos_controller_spawner,
+        robot_hand_controller_spawner,
+        fault_controller_spawner,
+        move_group_node,
+        static_tf,
+    ]
+    #############################################################################################
+
+
+
+
+    #############################################################################################
+    # dodao iz robot.launch.py
+    return nodes_to_start
+    #############################################################################################
+
+
+
+    """ return LaunchDescription(
+        [
+            rviz_node,
+            static_tf,
+            robot_state_publisher,
+            run_move_group_node,
+            ros2_control_node,
+        ]
+        + load_controllers
+    ) """
+
+
+
+
+
+#############################################################################################
+# dodao iz robot.launch.py
+def generate_launch_description():
+    # Declare arguments
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_ip",
+            description="IP address by which the robot can be reached.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_fake_hardware",
+            default_value="false",
+            description="Start robot with fake hardware mirroring command to its states.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "gripper_max_velocity",
+            default_value="100.0",
+            description="Max velocity for gripper commands",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "gripper_max_force",
+            default_value="100.0",
+            description="Max force for gripper commands",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_internal_bus_gripper_comm",
+            default_value="true",
+            description="Use arm's internal gripper connection",
+        )
+    )
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value="false",
+            description="Use simulated clock",
+        )
+    )
+
+    declared_arguments.append(
+        DeclareLaunchArgument("launch_rviz", default_value="true", description="Launch RViz?")
+    )
+
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
+#############################################################################################
